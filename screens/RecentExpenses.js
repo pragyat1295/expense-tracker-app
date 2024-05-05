6b523a323259bc2c838a105b004a0ee80780501c
pragyat1295 <pragyat1295@gmail.com>
2024-05-05 22:31:46 +0530

    added logic and data storage finished

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,14 +1,24 @@
-import { Text, StyleSheet } from 'react-native';
+import { useContext } from 'react';
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import { ExpenseContext } from '../store/expense-context';
+import { getDateMinusDays } from '../util/date';
 
 function RecentExpenses() {
-  return <Text style = {styles.textLayout} >RecentExpenses Screen</Text>;
+  const expensesCtx = useContext(ExpenseContext);
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return  expense.date >= date7DaysAgo && expense.date <= today;
+  });
+  return (
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 Days"
+      fallbackText="No expenses registered for the last 7 days."
+    />
+  );
 }
 
-export default RecentExpenses;
 
-const styles = StyleSheet.create({
-  textLayout: {
-    fontWeight: 'bold',
-    fontSize: 32,
-  }
-})
+export default RecentExpenses;
